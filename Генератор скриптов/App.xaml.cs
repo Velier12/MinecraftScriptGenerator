39183603b88a6571358fb120ed0bd9fae5ab9852
Fiarr4ik <fiarr4ik@gmail.com>
2024-05-21 09:16:11 +0700

    First

---
+++ Генератор скриптов/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Генератор_скриптов
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Генератор скриптов/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Генератор_скриптов
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
